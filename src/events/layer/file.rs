1f10469562a8cce3e97e8b3b0b22a984c7460726
George Makdesi <george.makdesi@icloud.com>
2024-11-24 02:35:28 +0100

    Added guard to avoid buffer-overflow on error, Added threshold

--- src/events/layer/file.rs
+++ src/events/layer/file.rs
@@ -1,11 +1,12 @@
 use std::marker::PhantomData;
 use std::path::PathBuf;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
 
 use tokio::fs::OpenOptions;
 use tokio::io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt};
-use tokio::sync::Mutex;
+use tokio::sync::{Mutex, Notify};
 
 use tracing::{Event, Subscriber};
 
@@ -16,6 +17,9 @@ use crate::events::record::{DefaultEvent, DefaultRecorder};
 use crate::events::traits::{EventRecorder, FileExtension, FileWriter};
 use crate::trace_error;
 
+/// File writer for CSV format.
+/// It writes events to a CSV file in a row format.
+/// Each event is serialized as a row with columns separated by commas.
 pub struct CSVFormat;
 
 impl FileExtension for CSVFormat {
@@ -27,6 +31,18 @@ impl FileExtension for CSVFormat {
 impl FileWriter for CSVFormat {
     type BufferType = DefaultEvent;
 
+    /// Writes the buffer to the file in CSV format.
+    ///
+    /// > **Note**: No internal strategy for handling errors when writing.
+    /// > It only returns an error when writing fails.
+    ///
+    /// # Parameters
+    /// - `buffer`: The buffer containing events to write.
+    /// - `file_path`: The path to the file to write the events. Extension is expected to be part of the file name.
+    ///
+    /// # Returns
+    /// - `Ok(())`: if the write operation is successful.
+    /// - `Err(io::Error)`: if the write operation has failed.
     async fn write(buffer: &[Self::BufferType], file_path: &PathBuf) -> tokio::io::Result<()> {
         // Open the file for appending (or create it if it doesn't exist)
         let mut file = OpenOptions::new()
@@ -34,13 +50,11 @@ impl FileWriter for CSVFormat {
             .append(true)
             .open(file_path)
             .await?;
-
         // Prepare CSV header if the file is new or empty
         if file.metadata().await?.len() == 0 {
             let header: &[u8; 38] = b"Level,Source,Message,Target,Timestamp\n";
             file.write_all(header).await?;
         }
-
         // Convert each entry in the buffer to a CSV string
         for entry in buffer {
             let csv_row = format!(
@@ -55,11 +69,13 @@ impl FileWriter for CSVFormat {
             // Write the CSV row to the file
             file.write_all(csv_row.as_bytes()).await?;
         }
-
         Ok(())
     }
 }
 
+/// File writer for JSON format.
+/// It writes events to a JSON file in an array format.
+/// Each event is serialized as a JSON object.
 pub struct JSONFormat;
 
 impl FileExtension for JSONFormat {
@@ -71,6 +87,18 @@ impl FileExtension for JSONFormat {
 impl FileWriter for JSONFormat {
     type BufferType = DefaultEvent;
 
+    /// Writes the buffer to the file in JSON format.
+    ///
+    /// > **Note**: No internal strategy for handling errors when writing.
+    /// > It only returns an error when writing fails.
+    ///
+    /// # Parameters
+    /// - `buffer`: The buffer containing events to write.
+    /// - `file_path`: The path to the file to write the events. Extension is expected to be part of the file name.
+    ///
+    /// # Returns
+    /// - `Ok(())`: if the write operation is successful.
+    /// - `Err(io::Error)`: if the write operation has failed.
     async fn write(buffer: &[Self::BufferType], file_path: &PathBuf) -> tokio::io::Result<()> {
         // Open/Create the file in read/write mode
         let mut file = OpenOptions::new()
@@ -79,7 +107,6 @@ impl FileWriter for JSONFormat {
             .create(true)
             .open(file_path)
             .await?;
-
         // Check if the file is new or empty
         if file.metadata().await?.len() == 0 {
             // If the file is empty, write the opening bracket
@@ -97,7 +124,6 @@ impl FileWriter for JSONFormat {
                 file.write_all(b",").await?; // Write a comma
             }
         }
-
         // Write entry in the buffer
         for (i, entry) in buffer.iter().enumerate() {
             // Serialize entry to bytes
@@ -112,22 +138,30 @@ impl FileWriter for JSONFormat {
                 file.write_all(b",\n").await?;
             }
         }
-
         // Add the closing bracket
         file.write_all(b"\n]").await?;
         Ok(())
     }
 }
 
+struct StoreData<T> {
+    // Async `Mutex` is required because there is no guarantee that
+    // the locking-time could be near-instant.
+    buffer: Mutex<Vec<T>>,
+    threshold: u16,
+    write_alert: Notify,
+    guard: AtomicBool,
+}
+
 /// File storage layer for tracing subscribers with memory buffer.
 /// It stores events in in-memory buffer and writes them to file in intervals.
 ///
-/// > **Note**: It writes all events to single file named `events.{extension}`,
+/// > **Note**: It writes all events to single file named events.{extension},
 /// but this might be changed in the future with more writing options.
 ///
 /// # Generic Parameters
 /// - `S`: The subscriber type to serve as layer.
-/// - `F`: The file format like `CSVFormat` and `JSONFormat`.
+/// - `F`: The file format like CSVFormat and JSONFormat.
 pub struct BufferedFileStore<S, F>
 where
     S: Subscriber,
@@ -135,7 +169,7 @@ where
 {
     _subscriber: PhantomData<S>,
     _format: PhantomData<F>,
-    buffer: Arc<Mutex<Vec<F::BufferType>>>,
+    data: Arc<StoreData<F::BufferType>>,
 }
 
 impl<S, F> BufferedFileStore<S, F>
@@ -143,46 +177,99 @@ where
     S: Subscriber,
     F: FileExtension + FileWriter,
 {
-    /// Creates new `BufferedFileStore`.
+    /// Creates new BufferedFileStore.
     ///
     /// # Parameters
     /// - `directory`: The directory where events files shall be stored.
     /// - `write_interval`: The waiting period before writing events to file.
-    pub fn new(mut directory: PathBuf, write_interval: Duration) -> Self
+    /// - `threshold`: The number of events to hold in the buffer before flushing to file, regardless of the interval.
+    /// - `allocate`: If true, the buffer will pre-allocate memory for `threshold` events.
+    pub fn new(
+        mut directory: PathBuf,
+        write_interval: Duration,
+        threshold: u16,
+        allocate: bool,
+    ) -> Self
     where
         <F as FileWriter>::BufferType: Send + 'static,
     {
-        let instance = Self {
-            _subscriber: PhantomData,
-            _format: PhantomData,
-            buffer: Arc::new(Mutex::new(Vec::new())),
-        };
-
+        // Shared data
+        let data = Arc::new(StoreData {
+            buffer: Mutex::new(if allocate {
+                Vec::with_capacity(threshold as usize)
+            } else {
+                Vec::new()
+            }),
+            guard: AtomicBool::new(false),
+            write_alert: Notify::new(),
+            threshold,
+        });
+        // Add the file name to the directory
         directory.push("events");
         directory.set_extension(F::extension()); // Compute once
-        let buffer_ref = instance.buffer.clone();
+        // Start the writer task
+        Self::write(directory, write_interval, data.clone());
+        // The new instance
+        Self {
+            _subscriber: PhantomData,
+            _format: PhantomData,
+            data,
+        }
+    }
 
-        // Move data and start detached task
+    fn write(directory: PathBuf, write_interval: Duration, data: Arc<StoreData<F::BufferType>>)
+    where
+        <F as FileWriter>::BufferType: Send + 'static,
+    {
         tokio::spawn(async move {
             loop {
-                tokio::time::sleep(write_interval).await;
-                let mut buffer = buffer_ref.lock().await;
-                if !buffer.is_empty() {
-                    // An error indicates a series I/O error
-                    if let Err(err) = F::write(&buffer, &directory).await {
-                        // Record the event for other subscribers and stop writing
+                if data.guard.load(Ordering::Acquire) {
+                    // This task is the last waiter to acquire the lock after the guard has been activated
+                    let mut buffer = data.buffer.lock().await;
+                    // TODO: No strategy for dealing with the current data in the buffer.
+                    // garbage collection
+                    buffer.clear();
+                    buffer.shrink_to(0);
+                    return; // Stop writing
+                }
+                // Mutex guard acquired here
+                let mut buffer = tokio::select! {
+                    _ = tokio::time::sleep(write_interval) =>
+                    {
+                        let buffer = data.buffer.lock().await;
+                        if buffer.is_empty() {
+                            continue;
+                        };
+                        buffer
+                    },
+                    _ = data.write_alert.notified() => {
+                        // The buffer has reached the threshold
+                        data.buffer.lock().await
+                    }
+                };
+                match F::write(&buffer, &directory).await {
+                    Ok(_) => {
+                        buffer.clear(); // Events are written, clear the buffer
+                    }
+                    // TODO: No strategy for handling errors when writing.
+                    Err(err) => {
+                        /*
+                        Since there is no strategy for handling errors when writing,
+                        we just stop writing and prevent further recordings, but
+                        tasks that are currently waiting to acquire the lock have passed the guard check already,
+                        so they will write the buffer.
+                        We allow one more iteration, to make sure this task is the last one acquiring the lock.
+                        */
+                        data.guard.store(true, Ordering::Release);
+                        // Propagate the event for other subscribers
                         trace_error!(
                             source = "BufferedFileStore",
                             message = format!("Stopped: {}", err)
                         );
-                        break;
                     }
-                    buffer.clear(); // Events are written, clear the buffer
-                }
+                };
             }
         });
-
-        instance
     }
 }
 
@@ -192,11 +279,24 @@ where
     F: FileExtension + FileWriter<BufferType = DefaultEvent> + 'static,
 {
     fn on_event(&self, event: &Event, _ctx: Context<S>) {
+        // Active guard means no writing, so we skip recording to prevent memory overflow
+        if self.data.guard.load(Ordering::Acquire) {
+            return;
+        }
         if let Some(schema) = DefaultRecorder::record(event) {
-            let buffer_ref = self.buffer.clone();
+            let data_ref = self.data.clone();
             tokio::spawn(async move {
-                let mut entries = buffer_ref.lock().await;
-                entries.push(schema);
+                // Wait to acquire the lock
+                let mut buffer = data_ref.buffer.lock().await;
+                // During waiting to acquire the lock, the guard might have been activated,
+                // but we let it write without rechecking anyway. Only new recordings will be skipped.
+                buffer.push(schema);
+                // This check must be done only after acquiring the lock and after pushing the event
+                if buffer.len() as u16 >= data_ref.threshold {
+                    // If the writer task is sleeping, it will wake up waiting for the lock to write the buffer,
+                    // otherwise, it will write the buffer as soon as it acquires the lock.
+                    data_ref.write_alert.notify_one();
+                }
             });
         }
     }
@@ -221,7 +321,9 @@ mod tests {
         // Buffered file layer with the log file path
         let store = BufferedFileStore::<Registry, CSVFormat>::new(
             PathBuf::from(""),
-            Duration::from_millis(10),
+            Duration::from_secs(60), // We rely on the threshold for this test
+            2, // Writing must be triggered after the second event is recorded
+            true,
         );
 
         // Initialize events subscriber with the layer
@@ -235,9 +337,9 @@ mod tests {
         trace_error!(source = "event 2", message = "error message");
 
         // Wait some time to ensure writing
-        tokio::time::sleep(Duration::from_millis(20)).await;
+        tokio::time::sleep(Duration::from_millis(10)).await;
 
-        // Full file path of the events file
+        // Full file path of the expected events file
         let cargo_manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
         let full_path = format!("{}/events.csv", cargo_manifest_dir);
 
@@ -302,7 +404,9 @@ mod tests {
         // Buffered file layer with the log file path
         let store = BufferedFileStore::<Registry, JSONFormat>::new(
             PathBuf::from(""),
-            Duration::from_millis(10),
+            Duration::from_secs(60), // We rely on the threshold for this test
+            2, // Writing must be triggered after the second event is recorded
+            true,
         );
 
         // Initialize events subscriber with the layer
@@ -316,9 +420,9 @@ mod tests {
         trace_error!(source = "event 2", message = "error message");
 
         // Wait some time to ensure writing
-        tokio::time::sleep(Duration::from_millis(20)).await;
+        tokio::time::sleep(Duration::from_millis(10)).await;
 
-        // Full file path of the events file
+        // Full file path of the expected events file
         let cargo_manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
         let full_path = format!("{}/events.json", cargo_manifest_dir);
 
@@ -392,5 +496,88 @@ mod tests {
         tokio::fs::remove_file(full_path)
             .await
             .expect("Failed to delete events file");
+    }
+
+    // Mock FileWriter that simulates a write error
+    struct MockFileWriter;
+
+    impl FileExtension for MockFileWriter {
+        fn extension() -> &'static str {
+            "mock"
+        }
+    }
+
+    impl FileWriter for MockFileWriter {
+        type BufferType = DefaultEvent;
+
+        async fn write(_buffer: &[Self::BufferType], _file_path: &PathBuf) -> tokio::io::Result<()> {
+            Err(tokio::io::Error::new(tokio::io::ErrorKind::Other, "Simulated write error"))
+        }
+    }
+
+    struct AtomicStoreReference {
+        store: Arc<BufferedFileStore<Registry, MockFileWriter>>,
+    }
+
+    impl AtomicStoreReference {
+        fn new(store: BufferedFileStore<Registry, MockFileWriter>) -> Self {
+            Self {
+                store: Arc::new(store),
+            }
+        }
+    }
+
+    impl Layer<Registry> for AtomicStoreReference
+    {
+        fn on_event(&self, event: &Event, _ctx: Context<Registry>) {
+            self.store.on_event(event, _ctx);
+        }
+    }
+
+    impl Clone for AtomicStoreReference {
+        fn clone(&self) -> Self {
+            Self {
+                store: self.store.clone(),
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_buffered_store_write_error_handling() {
+        // Buffered file layer with the log file path
+        let store = BufferedFileStore::<Registry, MockFileWriter>::new(
+            PathBuf::from(""),
+            Duration::from_secs(60), // We rely on the threshold for this test
+            2, // Writing must be triggered after the second event is recorded
+            true,
+        );
+
+        let store_ref = AtomicStoreReference::new(store);
+
+        // Initialize events subscriber with the layer
+        let subscriber = Registry::default().with(store_ref.clone());
+
+        // Make subscriber a valid default for the entire test
+        let _guard = subscriber::set_default(subscriber);
+
+        // Propagate two events to reach the threshold of 2
+        trace_info!(source = "event 1", message = "info message");
+        trace_error!(source = "event 2", message = "error message");
+
+        // Wait some time for writing to be triggered
+        tokio::time::sleep(Duration::from_millis(10)).await;
+
+        // Recording guard must have been activated as a result of the write error
+        assert!(store_ref.store.data.guard.load(Ordering::Acquire));
+
+        // Garbage collection must have been done
+        assert!(store_ref.store.data.buffer.lock().await.is_empty());
+        assert_eq!(store_ref.store.data.buffer.lock().await.capacity(), 0);
+
+        // Try to propagate another event
+        trace_info!(source = "event 3", message = "info message");
+
+        // No recording should have been done
+        assert!(store_ref.store.data.buffer.lock().await.is_empty());
     }
 }
